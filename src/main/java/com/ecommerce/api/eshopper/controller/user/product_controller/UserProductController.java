661822e0ce0eccdb95ba711333606ba1363a4e89
rezarria <votanamcb@outlook.com>
2023-11-15 16:46:07 +0700

    chỉnh view shop

--- src/main/java/com/ecommerce/api/eshopper/controller/user/product_controller/UserProductController.java
+++ src/main/java/com/ecommerce/api/eshopper/controller/user/product_controller/UserProductController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.api.eshopper.controller.user.product_controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserProductController {
 
     @GetMapping("/shop")
-    public String userShop() {
+    public String userShop(Model model) {
+        model.addAttribute("page", 0);
+        model.addAttribute("size", 0);
         return "user/shop";
     }
 }
